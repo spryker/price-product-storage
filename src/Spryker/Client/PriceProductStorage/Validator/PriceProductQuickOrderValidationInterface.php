6807d5e12c6c9d60d83b755215a300a3eb26b7d9
Ivan Pugach <ivan.pugach@spryker.com>
2019-01-11 17:58:11 +0200

    Fix after CR

--- src/Spryker/Client/PriceProductStorage/Validator/PriceProductQuickOrderValidationInterface.php
+++ src/Spryker/Client/PriceProductStorage/Validator/PriceProductQuickOrderValidationInterface.php
@@ -5,11 +5,11 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Client\PriceProductStorage\Plugin\QuickOrder\Validator;
+namespace Spryker\Client\PriceProductStorage\Validator;
 
 use Generated\Shared\Transfer\QuickOrderTransfer;
 
-interface PriceProductQuickOrderValidatorInterface
+interface PriceProductQuickOrderValidationInterface
 {
     /**
      * @param \Generated\Shared\Transfer\QuickOrderTransfer $quickOrderTransfer
